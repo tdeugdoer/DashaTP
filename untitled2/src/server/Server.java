259a04ff06195e93d17036f7f42e9ed05db68a9a
tdeugdoer <tegor2003@gmail.com>
2024-03-17 23:46:50 +0300

    Надоело, чтобы не висело пушу

--- untitled2/src/server/Server.java
+++ untitled2/src/server/Server.java
@@ -3,7 +3,6 @@ package server;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Server {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(5050)) {
